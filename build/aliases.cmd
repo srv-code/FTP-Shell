4bbc105f633323e4cb449d40e4eb45077d0a7398
Sourav Dey <sourav.blackjack@gmail.com>
2020-01-08 01:05:10 +0530

    updated aliases.cmd

--- build/aliases.cmd
+++ build/aliases.cmd
@@ -1,5 +1,5 @@
 [COMPILE AND RUN]
-javac -d build/classes/ -cp "W:\commons-net-3.6.jar"  src/fs/ftp/*.java src/fs/ftp/handler/*.java && java -cp "W:\commons-net-3.6.jar;build/classes/" fs.ftp.FTPShell --debug
+javac -d out -cp "W:\commons-net-3.6.jar" src/fs/ftp/shell/StandardExitCodes.java src/fs/ftp/shell/FTPShell.java src/fs/ftp/handler/FTPHandler.java && java -cp "W:\commons-net-3.6.jar;out/" fs.ftp.shell.FTPShell --debug
 
 [SIMPLY RUN]
-java -cp "W:\commons-net-3.6.jar;build/classes/" fs.ftp.FTPShell --debug
+java -cp "W:\commons-net-3.6.jar;out/" fs.ftp.shell.FTPShell --debug
